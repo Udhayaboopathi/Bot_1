316cbc3ce1b85f13ab6a8f80ca2f77cda19a9da0
Udhayaboopathi <udhayayboopathi2003@gmail.com>
2024-06-15 00:45:32 +0530

    tocan

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   prefix: "!",
   owner: "581142001739628565",
   token:
-    "MTEyNTgwNTg2MTExODQzMTMyMg.GA-83k.fs0E0ifRf8MoQzB-qwcJ_VRZohqwKOtVrHgzAk",
+    "MTEyNTgwNTg2MTExODQzMTMyMg.GFIZ1j.CbFrWP5GVljV8U-WKLG3HRCsvXsIqqAJ4aZe5s",
 };
